05bb95f097f580f87f62612f443ea9e28d802809
Rena Hashimi <renhas54143@stud.noroff.no>
2023-10-13 18:08:13 +0200

    ADD FILES

---
+++ js/product.js
@@ -0,0 +1,9 @@
+import products from './productDetails.js';
+
+
+//Product.html
+var productTitle = document.getElementById("productname");
+var productPrice = document.getElementById(".price");
+var productProperties = document.getElementById("productprops");
+var productImage = document.getElementById("productimg");
+let cartButton = document.getElementById("atc");
